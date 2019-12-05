3e1722c80008b9c6a4fa5522edfcddea2445d2ec
0624006 <55527041+0624006@users.noreply.github.com>
2019-12-05 15:18:51 +0800

    add homework4_1

---
+++ homework4_1.java
@@ -0,0 +1,12 @@
+class Student {
+    Course[] courses = new Course[10];
+}
+class Course {
+}
+
+public class homework4_1{
+    public static void main(String args[]){
+        Student student = new Student();
+        Course course = new Course();
+    }
+}
